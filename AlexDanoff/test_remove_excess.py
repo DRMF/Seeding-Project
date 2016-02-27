2c9c54978751aee2715e1848a975046b6a5db505
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2016-02-27 18:51:15 +0100

    reorganized the structure

--- AlexDanoff/test_remove_excess.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from remove_excess import remove_section
-
-
-class TestRemoveExcess(TestCase):
-    def test_remove_section(self):
-        content = 'AABegin This should be removed BeginDDBB'
-        result = remove_section(r'Begin', r'Begin', content)
-        self.assertEqual('AABeginDDBB', result)
