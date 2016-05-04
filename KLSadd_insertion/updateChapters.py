e1235cc8294fd241134d2eecfcc2a4bb8af3e2ac
Edward Bian <eb092012@gmail.com>
2016-05-04 17:41:42 -0400

    New indexes

--- KLSadd_insertion/updateChapters.py
+++ KLSadd_insertion/updateChapters.py
@@ -17,6 +17,9 @@ Additional goals (not already addressed in linetest.py):
 -rewrite for "smarter" edits (ex. add new limit relations straight to the limit relations section in the section itself, not at the end)
 Current update status: incomplete
 Goals:change the book chapter files to include paragraphs from the addendum, and after that insert some edits so they are intelligently integrated into the chapter
+
+Edward Bian
+Currently under heavy modification; sections may not work and/or look inefficient/confusing
 """
 
 #start out by reading KLSadd.tex to get all of the paragraphs that must be added to the chapter files
@@ -25,11 +28,17 @@ Goals:change the book chapter files to include paragraphs from the addendum, and
 #variables: chapNums for the chapter number each section belongs to paras will hold the sections that will be copied over
 #chapNums is needed to know which file to open (9 or 14)
 #mathPeople is just the name for the sections that are used, like Wilson, Racah, etc. I know some are not people, its just a var name
-#yes I like lists
 chapNums = []
 paras = []
+klsparas = []
 mathPeople = []
 newCommands = [] #used to hold the indexes of the commands
+ref9II = [] #Hold section search indexes
+ref14II = []
+ref9III = [] #Holds all indexes
+ref14III = []
+specref9 = []
+specref14 = []
 comms = "" #holds the ACTUAL STRINGS of the commands
 #2/18/16 this method addresses the goal of hardcoding in the necessary packages to let the chapter files run as pdf's.
 #Currently only works with chapter 9, ask Dr. Cohl to help port your chapter 14 output file into a pdf
@@ -80,14 +89,14 @@ def insertCommands(kls, chap, cms):
 def findReferences(chapter):
     references = []
     index = -1
-    chaptercheck = 0
     #chaptercheck designates which chapter is being searched for references
+    chaptercheck = 0
     if chapticker == 0:
         chaptercheck = str(9)
     elif chapticker == 1:
         chaptercheck = str(14)
-    canAdd = False
     #canAdd tells the program whether the next section is a reference
+    canAdd = False
     for word in chapter:
         index+=1
         #check sections and subsections
@@ -96,49 +105,88 @@ def findReferences(chapter):
             ws = word[word.find("{")+1: word.find("~")]
             for unit in mathPeople:
                 subunit = unit[unit.find(" ")+1: unit.find("#")]
+                # System of checks that verifies if section is in chapter
                 if ((w in subunit) or (ws in subunit)) and (chaptercheck in unit) and (len(w) == len(subunit)) or (("Pseudo Jacobi" in w) and ("Pseudo Jacobi (or Routh-Romanovski)" in subunit)):
                     canAdd = True
-                    #System of checks that verifies if section is in chapter
+                    if chapticker == 0:
+                        ref9II.append(index)
+                        ref9III.append(index)
+                    elif chapticker == 1:
+                        ref14II.append(index)
+                        ref14III.append(index)
         if("\\subsection*{References}" in word) and (canAdd == True):
+            # Appends valid locations
             references.append(index)
+            if chapticker == 0:
+                ref9II.append(index)
+                ref9III.append(index)
+            elif chapticker == 1:
+                ref14II.append(index)
+                ref14III.append(index)
             canAdd = False
-            #Appends valid locations
+        if ("subsection*{" in word and "References" not in word):
+            if chapticker == 0:
+                ref9III.append(index)
+            elif chapticker == 1:
+                ref14III.append(index)
+    print(ref9II)
+    print (ref14II)
+    print(ref9III)
+    print(ref14III)
     return references
 
-#method to change file string(actually a list right now), returns string to be written to file
-#If you write a method that changes something, it is preffered that you call the method in here
-def fixChapter(chap, references, p, kls):
-    #chap is the file string(actually a list), references is the specific references for the file,
-    #and p is the paras variable(not sure if needed) kls is the KLSadd.tex as a list
-    count = 0 #count is used to represent the values in count
-    designator = 0 #Tells which chapter it's on
+def referencePlacer(chap, references, p, kls,refII,refIII,specref):
+    # count is used to represent the values in count
+    count = 0
+    # Tells which chapter it's on
+    designator = 0
     if chapticker2 == 0:
         designator = "9."
     elif chapticker2 == 1:
         designator = "14."
     for i in references:
-        #Place before References paragraph
+        # Place before References paragraph
         word1 = str(p[count])
-        if (designator in word1[word1.find("\\subsection*{") + 1: word1.find("}") ]):
-            chap[i-2] += "%Begin KLSadd additions"
-            chap[i-2] += p[count]
-            chap[i-2] += "%End of KLSadd additions"
+        if (designator in word1[word1.find("\\subsection*{") + 1: word1.find("}")]):
+            chap[i - 2] += "%Begin KLSadd additions"
+            chap[i - 2] += p[count]
+            chap[i - 2] += "%End of KLSadd additions"
             count += 1
         else:
-            while (designator not in word1[word1.find("\\subsection*{") + 1: word1.find("}") ]):
+            while (designator not in word1[word1.find("\\subsection*{") + 1: word1.find("}")]):
                 word1 = str(p[count])
-                if (designator in word1[word1.find("\\subsection*{") + 1: word1.find("}") ]):
+                if (designator in word1[word1.find("\\subsection*{") + 1: word1.find("}")]):
                     chap[i - 2] += "%Begin KLSadd additions"
                     chap[i - 2] += p[count]
                     chap[i - 2] += "%End of KLSadd additions"
                     count += 1
                 else:
-                    count+=1
-            #Unfinished code segment that inserts the collected paras into the chapter
+                    count += 1
+
+def referencePlacerII(chap, references, p, kls,refII,refIII,specref):
+    if chapticker2 == 0:
+        designator = "9."
+    elif chapticker2 == 1:
+        designator = "14."
+    count = 0
+    sectionnuma = specref[0]
+    sectionnumb = specref[1]
+    for i in specref:
+        pass
+
+
+
+#method to change file string(actually a list right now), returns string to be written to file
+#If you write a method that changes something, it is preffered that you call the method in here
+def fixChapter(chap, references, p, kls,refII,refIII,specref):
+    #chap is the file string(actually a list), references is the specific references for the file,
+    #and p is the paras variable(not sure if needed) kls is the KLSadd.tex as a list
+    referencePlacer(chap, references, p, kls, refII,refIII,specref)
     chap = prepareForPDF(chap)
     cms = getCommands(kls)
     chap = insertCommands(kls,chap, cms)
     commentticker = 0
+    # Hard coded command remover
     for word in chap:
         word2 = chap[chap.index(word)-1]
         if ("\\newcommand{\qhypK}[5]{\,\mbox{}_{#1}\phi_{#2}\!\left(" not in word2):
@@ -155,19 +203,19 @@ def fixChapter(chap, references, p, kls):
                 wordtoadd = "%" + word
                 chap[commentticker] = wordtoadd
         commentticker += 1
-        #Hard coded command remover
     ticker1 = 0
+    # Formatting to make the Latex file run
     while ticker1 < len(chap):
         if ('\\myciteKLS' in chap[ticker1]):
             chap[ticker1] = chap[ticker1].replace('\\myciteKLS', '\\cite')
         ticker1 += 1
-        #Formatting to make the Latex file run
     return chap
 
 #open the KLSadd file to do things with
 with open("KLSadd.tex", "r") as add:
     #store the file as a string
     addendum = add.readlines()
+    #Makes sections look like other sections
     for word in addendum:
         if ("paragraph{" in word):
             lenword = len(word) - 1
@@ -176,9 +224,10 @@ with open("KLSadd.tex", "r") as add:
         if ("subsubsection*{" in word):
             lenword = len(word) - 1
             addendum[addendum.index(word)] = word[0:word.find("{") + 1] + "\large\\bf KLSadd: " + word[word.find("{") + 1: lenword]
-        #Font processing
     index = 0
     indexes = []
+    # Designates sections that need stuff added
+    # get the index
     for word in addendum:
         index+=1
         if("." in word and "\\subsection*{" in word):
@@ -186,13 +235,20 @@ with open("KLSadd.tex", "r") as add:
                 chapNums.append(9)
                 name = word[word.find("{") + 1: word.find("}") ]
                 mathPeople.append(name + "#")
+                specref9.append(index-1)
             if("14." in word):
                 chapNums.append(14)
                 name = word[word.find("{") + 1: word.find("}") ]
                 mathPeople.append(name + "#")
-            #Designates sections that need stuff added
-            #get the index
+                specref14.append(index - 1)
             indexes.append(index-1)
+        if ("paragraph{" in word) and (index > 313):
+            klsparas.append(index-1)
+    print(indexes)
+    print(specref9)
+    print(specref14)
+    print(klsparas)
+    print(mathPeople)
     #now indexes holds all of the places there is a section
     #using these indexes, get all of the words in between and add that to the paras[]
     for i in range(len(indexes)-1):
@@ -224,9 +280,9 @@ with open("KLSadd.tex", "r") as add:
     references14 = findReferences(entire14)
     #call the fixChapter method to get a list with the addendum paragraphs added in
     chapticker2 = 0
-    str9 = ''.join(fixChapter(entire9, references9, paras, addendum))
+    str9 = ''.join(fixChapter(entire9, references9, paras, addendum,ref9II,ref9III,specref9))
     chapticker2 += 1
-    str14 = ''.join(fixChapter(entire14, references14, paras, addendum))
+    str14 = ''.join(fixChapter(entire14, references14, paras, addendum,ref14II,ref14III,specref14))
 
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
