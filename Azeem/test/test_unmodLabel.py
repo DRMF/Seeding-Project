1becd2d009efa61a7b1902aea713e6b231c8bf55
pzw <philipwang2000@gmail.com>
2016-06-16 14:22:13 -0400

    Remove redundant test cases test_unmodLabel

--- Azeem/test/test_unmodLabel.py
+++ Azeem/test/test_unmodLabel.py
@@ -4,7 +4,5 @@ from tex2Wiki import setup_label_links
 class TestModLabel(TestCase):
 
     def test_unmodLabel(self):
-        self.assertEqual('Formula:DLMF:25.2:E1', unmodLabel('Formula:DLMF:25.2:E1'))
-        self.assertEqual('Formula:DLMF:15.2:E1', unmodLabel('Formula:DLMF:15.02:E1'))
-        self.assertEqual('Formula:DLMF:5.12:E1', unmodLabel('Formula:DLMF:05.12:E1'))
-        self.assertEqual('Formula:DLMF:5.2:E1', unmodLabel('Formula:DLMF:05.02:E1'))
+        self.assertEqual('Formula:DLMF:15.2:E1', unmodLabel('Formula:DLMF:15.02:E1')) #remove 0's before decimal point
+        self.assertEqual('Formula:DLMF:5.2:E1', unmodLabel('Formula:DLMF:05.02:E1')) #remove 0's before both decimal points and after colon
