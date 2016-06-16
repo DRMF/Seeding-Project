e6cec17a1e580ef4779267384fbdcfab3825b8a9
pzw <philipwang2000@gmail.com>
2016-06-16 12:35:24 -0400

    Remove Line: placeholder in test cases for test_getEq

--- Azeem/test/test_getEq.py
+++ Azeem/test/test_getEq.py
@@ -4,6 +4,6 @@ from tex2Wiki import setup_label_links
 class TestGetEq(TestCase):
 
     def test_getEq(self):
-        self.assertEqual('<math>\realpart{s} > 1</math>', getEq('Line: %  \constraint{$\realpart{s} > 1$}'))
-        self.assertEqual('<math>\realpart{s} > -1</math> &<br /> <math>s\neq 1</math>', getEq(' Line: %  \constraint{$\realpart{s} > -1$ &<br /> $s \neq 1$}\n'))
-        self.assertEqual('<math>\realpart{s} > -1</math> &<br /> <math>s\neq 1</math>', getEq('\n Line: %  \constraint{$\realpart{s} > -1$ &<br /> $s \neq 1$}'))
+        self.assertEqual('<math>\realpart{s} > 1</math>', getEq('%  \constraint{$\realpart{s} > 1$}'))
+        self.assertEqual('<math>\realpart{s} > -1</math> &<br /> <math>s\neq 1</math>', getEq('%  \constraint{$\realpart{s} > -1$ &<br /> $s \neq 1$}\n'))
+        self.assertEqual('<math>\realpart{s} > -1</math> &<br /> <math>s\neq 1</math>', getEq('%  \constraint{$\realpart{s} > -1$ &<br /> $s \neq 1$}'))
