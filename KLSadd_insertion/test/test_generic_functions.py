fb5cd99c594985425d796b24300cd997086ef4ed
Edward Bian <eb092012@gmail.com>
2016-08-16 13:36:13 -0400

    More Unit Tests

--- KLSadd_insertion/test/test_generic_functions.py
+++ KLSadd_insertion/test/test_generic_functions.py
@@ -5,7 +5,7 @@ __status__ = 'Development'
 from unittest import TestCase
 from updateChapters import new_keywords
 from updateChapters import extraneous_section_deleter
-
+from updateChapters import cut_words
 
 class TestNewKeywords(TestCase):
 
@@ -15,6 +15,17 @@ class TestNewKeywords(TestCase):
 
 class TestExtraneousSectionDeleter(TestCase):
 
-    def testExtraneousSectionDeleter(self):
+    def test_extraneous_section_deleter(self):
         self.assertEquals(extraneous_section_deleter(['reference', 'bananas', 'limit relation', '458673',
             'limit relations', 'apple']), ['bananas', '458673', 'apple'])
+
+
+class TestCutWords(TestCase):
+
+    def test_cut_words(self):
+        self.assertEquals(cut_words('MoreWords','WordsAndSomeMoreWords'), 'WordsAndSome')
+        self.assertEquals(cut_words('SupposedToFail', 'WordsAndSomeMoreWords'), 'WordsAndSomeMoreWords')
+        self.assertEquals(cut_words('StuffFromAddenum', '''\\paragraph{\\large\\bf KLSadd: TestCase}StuffFromAddenum\\begin{equation}\\end{equation}'''),
+                                                        '''\\begin{equation}\\end{equation}''')
+        #self.assertEquals(extraneous_section_deleter(['reference', 'bananas', 'limit relation', '458673',
+        #     'limit relations', 'apple']), ['bananas', '458673', 'apple'])
