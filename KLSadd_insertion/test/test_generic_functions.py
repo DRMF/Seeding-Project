5c255b39fd85894b0c5f5ea60dfaa2fedbb5649f
Edward Bian <eb092012@gmail.com>
2016-08-18 16:39:11 -0400

    Small Fixes and updates

--- KLSadd_insertion/test/test_generic_functions.py
+++ KLSadd_insertion/test/test_generic_functions.py
@@ -6,7 +6,6 @@ from unittest import TestCase
 from updateChapters import new_keywords
 from updateChapters import extraneous_section_deleter
 from updateChapters import cut_words
-from updateChapters import reference_formatter
 
 class TestNewKeywords(TestCase):
 
@@ -30,21 +29,4 @@ class TestCutWords(TestCase):
         self.assertEquals(cut_words('StuffFromAddenum', '''\\paragraph{\\large\\bf KLSadd: TestCase}StuffFromAddenum\\begin{equation}\\end{equation}'''),
                                                         '''\\begin{equation}\\end{equation}''')
 
-class TestReferenceFormatter(TestCase):
 
-    def test_reference_formatter(self):
-        self.assertEquals(reference_formatter(['by \eqref{1337}, This should work', 'WordsThatMightBeFoundInTheChapter'])
-                                            , ['by \eqref{KLSadd: 1337}, This should work',
-                                            'WordsThatMightBeFoundInTheChapter'])
-        self.assertEquals(reference_formatter(['by \eqref{1337} and \eqref{360} This should work', 'WordsThatMightBeFoundInTheChapter'])
-                                            , ['by \eqref{KLSadd: 1337} and \eqref{KLSadd: 360} This should work',
-                                            'WordsThatMightBeFoundInTheChapter'])
-        self.assertEquals(reference_formatter(['by \eqref{1337}, \eqref{720} and \eqref{360} This should work', 'WordsThatMightBeFoundInTheChapter'])
-                                            , ['by \eqref{KLSadd: 1337}, \eqref{KLSadd: 720} and \eqref{KLSadd: 360} This should work',
-                                            'WordsThatMightBeFoundInTheChapter'])
-        self.assertEquals(reference_formatter(['by \eqref{1337}, \eqref{720}, \eqref{500} and \eqref{360} This should work', 'WordsThatMightBeFoundInTheChapter'])
-                                            , ['by \eqref{KLSadd: 1337}, \eqref{KLSadd: 720}, \eqref{KLSadd: 500} and \eqref{KLSadd: 360} This should work',
-                                            'WordsThatMightBeFoundInTheChapter'])
-        self.assertEquals(reference_formatter(['by \eqref{1337}, \eqref{720}, \eqref{500}, \eqref{404} and \eqref{360} This should work', 'WordsThatMightBeFoundInTheChapter'])
-                                            , ['by \eqref{KLSadd: 1337}, \eqref{KLSadd: 720}, \eqref{KLSadd: 500}, \eqref{KLSadd: 404} and \eqref{KLSadd: 360} This should work',
-                                            'WordsThatMightBeFoundInTheChapter'])
