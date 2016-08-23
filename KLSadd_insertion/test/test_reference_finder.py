8a296a95712f0a3f625f4dcf5ffa9d55021354d2
Edward Bian <eb092012@gmail.com>
2016-08-23 14:38:33 -0400

    Unit Tests

--- KLSadd_insertion/test/test_reference_finder.py
+++ KLSadd_insertion/test/test_reference_finder.py
@@ -14,32 +14,32 @@ class TestFindReferences(TestCase):
                                             , '& &{}=\hyp{4}{3}{-n,n+\alpha+\beta+1,-x,', '\end{eqnarray}'
                                             , 'with $N$ a nonnegative integer.', '\subsection*{References}',
                                             '\cite{Askey89I}, \cite{AskeyWilson79}, \cite{AskeyWilson85}'],
-                                            0, ['9.9 Pseudo Jacobi (or Routh-Romanovski)#']), ([8], [0, 1, 8, 2646], []))
+                                            0, ['9.9 Pseudo Jacobi (or Routh-Romanovski)#']), ([8], [0, 1, 8], []))
         self.assertEquals(find_references(['\\section{Pseudo Jacobi}\\index{Racah polynomials}',
                                            '\\subsection*{Hypergeometric representation}', '\\begin{eqnarray}',
                                            '\\label{DefRacah}', '& &R_n(\lambda(x);\alpha,\beta,\gamma,\delta)\nonumber\\'
                                             , '& &{}=\hyp{4}{3}{-n,n+\alpha+\beta+1,-x,', '\end{eqnarray}'
                                             , 'with $N$ a nonnegative integer.', '\subsection*{References}',
                                             '\cite{Askey89I}, \cite{AskeyWilson79}, \cite{AskeyWilson85}'],
-                                            1, ['9.9 Pseudo Jacobi (or Routh-Romanovski)#']), ([8], [2646], [0, 1, 8]))
+                                            1, ['9.9 Pseudo Jacobi (or Routh-Romanovski)#']), ([8], [], [0, 1, 8]))
         self.assertEquals(find_references(['\\section{Racah}\\index{Racah polynomials}',
                                            '\\subsection*{Hypergeometric representation}', '\\begin{eqnarray}',
                                            '\\label{DefRacah}', '& &R_n(\lambda(x);\alpha,\beta,\gamma,\delta)\nonumber\\'
                                             , '& &{}=\hyp{4}{3}{-n,n+\alpha+\beta+1,-x,', '\end{eqnarray}'
                                             , 'with $N$ a nonnegative integer.', '\subsection*{References}',
                                             '\cite{Askey89I}, \cite{AskeyWilson79}, \cite{AskeyWilson85}'],
-                                            0, ['9.2 Racah#']), ([8], [0, 1, 8, 2646], []))
+                                            0, ['9.2 Racah#']), ([8], [0, 1, 8], []))
         self.assertEquals(find_references(['\\section{Racah}\\index{Racah polynomials}',
                                            '\\subsection*{Hypergeometric representation}', '\\begin{eqnarray}',
                                            '\\label{DefRacah}', '& &R_n(\lambda(x);\alpha,\beta,\gamma,\delta)\nonumber\\'
                                             , '& &{}=\hyp{4}{3}{-n,n+\alpha+\beta+1,-x,', '\end{eqnarray}'
                                             , 'with $N$ a nonnegative integer.', '\subsection*{References}',
                                             '\cite{Askey89I}, \cite{AskeyWilson79}, \cite{AskeyWilson85}'],
-                                            0, ['9.2 Racah#']), ([8], [0, 1, 8, 2646], []))
+                                            0, ['9.2 Racah#']), ([8], [0, 1, 8], []))
         self.assertEquals(find_references(['\\section{Unique}\\index{Racah polynomials}',
                                            '\\subsection*{Hypergeometric representation}', '\\begin{eqnarray}',
                                            '\\label{DefRacah}', '& &R_n(\lambda(x);\alpha,\beta,\gamma,\delta)\nonumber\\'
                                             , '& &{}=\hyp{4}{3}{-n,n+\alpha+\beta+1,-x,', '\end{eqnarray}'
                                             , 'with $N$ a nonnegative integer.', '\subsection*{References}',
                                             '\cite{Askey89I}, \cite{AskeyWilson79}, \cite{AskeyWilson85}'],
-                                            1, ['9.2 Racah#']), ([], [2646], [0, 1]))
+                                            1, ['9.2 Racah#']), ([], [], [0, 1]))
