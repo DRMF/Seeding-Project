af111853d318c589e0878fb1c3343255218890f8
Edward Bian <eb092012@gmail.com>
2016-12-20 16:45:53 -0500

    Refactored unit tests

--- KLSadd_insertion/test/test_chap1_placer.py
+++ KLSadd_insertion/test/test_chap1_placer.py
@@ -3,14 +3,14 @@ __author__ = 'Edward Bian'
 __status__ = 'Development'
 
 from unittest import TestCase
-from updateChapters import chap1placer
+from updateChapters import chap_1_placer
 
 
 class TestChap1Placer(TestCase):
 
     def test_chap1_placer(self):
-        self.assertEquals(chap1placer(['WordsAndSomeMoreWords','SampleEquation', '\\end{document}']
-                                    , ['\\subsection*{Generalities}', '\\paragraph{MathFunction}', 'MathEquations', 'WordsAndStuff',
+        self.assertEquals(chap_1_placer(['WordsAndSomeMoreWords', 'SampleEquation', '\\end{document}']
+                                        , ['\\subsection*{Generalities}', '\\paragraph{MathFunction}', 'MathEquations', 'WordsAndStuff',
                                        '\\subsection*{9.1 Wilson}', '\\paragraph{Symmetry}', 'WordsAndStuff'], [0,1,4,5])
-                                    , 'WordsAndSomeMoreWordsSampleEquation\\paragraph{\\bf KLS Addendum: Generalities}\\paragraph{MathFunction}MathEquationsWordsAndStuff\\end{document}')
+                          , 'WordsAndSomeMoreWordsSampleEquation\\paragraph{\\bf KLS Addendum: Generalities}\\paragraph{MathFunction}MathEquationsWordsAndStuff\\end{document}')
 
